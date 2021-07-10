3e9f4970d98615feafb531f8e0e7c1743d82a753
auburn <auburnete@gmail.com>
2021-07-10 02:13:22 +0000

    add DEV_GUILD env variable
     - for faster updates
     - also remove set_slash_commands(), turns out PUT /commands is already idempotent

--- ob/register.py
+++ ob/register.py
@@ -5,17 +5,18 @@ from .constants import BOT_TOKEN, APPLICATION_ID, DISCORD_API_URL, DEV_GUILD
 
 bot_auth = {'Authorization': f"Bot {BOT_TOKEN}"}
 
+# DEV_GUILD is False if env variable not defined
 if DEV_GUILD:
-    base_url = f"{DISCORD_API_URL}/applications/{APPLICATION_ID}/commands"
+    base_url = f"{DISCORD_API_URL}/applications/{APPLICATION_ID}/guilds/{DEV_GUILD}/commands"
 else:
     base_url = f"{DISCORD_API_URL}/applications/{APPLICATION_ID}/commands"
 
 """
-Get a list of all current GLOBAL slash commands.
+Get a list of all current slash commands.
 """
-async def current_global_slash_commands():
+async def current_slash_commands():
     async with httpx.AsyncClient() as client:
-        r = await client.get(f"{DISCORD_API_URL}/applications/{APPLICATION_ID}/commands", headers=bot_auth)
+        r = await client.get(base_url, headers=bot_auth)
     return r.json()
 
 """
@@ -23,7 +24,7 @@ Remove a global slash command.
 """
 async def remove_slash_command(id):
     async with httpx.AsyncClient() as client:
-        r = await client.delete(f"{DISCORD_API_URL}/applications/{APPLICATION_ID}/commands/{id}", headers=bot_auth)
+        r = await client.delete(f"{base_url}/{id}", headers=bot_auth)
     return r
 
 """
@@ -31,26 +32,7 @@ update slash commands
 """
 async def update_slash_commands(commands):
     async with httpx.AsyncClient() as client:
-        r = await client.put(f"{DISCORD_API_URL}/applications/{APPLICATION_ID}/commands", json=commands, headers=bot_auth)
+        r = await client.put(f"{base_url}/commands", json=commands, headers=bot_auth)
     return r
 
 
-"""
-mass update the current GLOBAL slash commands (idempotent)
-"""
-async def set_global_slash_commands(commands):
-    current_commands = await current_global_slash_commands()
-    command_names = [c["name"] for c in commands]
-
-    # anything that exists currently that's not in the ones we're adding...
-    to_remove = [c["id"] for c in current_commands if c["name"] not in command_names]
-
-    # loop through deleting all them:
-    for id in to_remove:
-        await remove_slash_command(id)
-
-    # then add!
-    await update_slash_commands(commands)
-
-    # and we'll return the newly updated commands...
-    return await current_global_slash_commands()
