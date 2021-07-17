aa563b39e415efbad7ca03393db17b8014979959
auburn <auburnete@gmail.com>
2021-07-17 12:41:52 +0000

    dang copilot is pretty cool ngl

--- ob/slash_router.py
+++ ob/slash_router.py
@@ -26,14 +26,24 @@ class SlashRoute:
 class SlashRouter:
     def __init__(self, routes):
         self._routes = routes
+        # build a mapping of route names to routes
+        self._route_map = {}
+        for route in self._routes:
+            self._route_map[route._name] = route
 
     def api(self):
         return [r.api() for r in self._routes]
 
     def handle(self, body):
-        return JSONResponse({
-            "type" : 4,
-            "data" : {
-                "content": "pong!"
-            }
-        })
+        # Get the route name from the body. The body is a dictionary. The route name is located under the key "data.name"
+        route_name = body['data']['name']
+        # Check if the route name is present in the mapping.
+        if route_name in self._route_map:
+            # Execute the associated handler.
+            return self._route_map[route_name]._handler(body)
+        else:
+            # Otherwise, return a default response. The default response has a 200 return code.
+            # The key 'type' is 4, and the key 'data.content' is something like "oh no"
+            return JSONResponse({"type": 4, "data": {"content": "oh no"}})
+
+
